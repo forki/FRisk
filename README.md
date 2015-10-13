5e57646a241d78626a07538ab0b8dd2ea2230daf
markstownsend <markstownsend@yahoo.com>
2015-10-13 22:30:29 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+
+# FRisk
+
+This project is an aide memoire for me for financial risk topics.    
+Read the [Tutorial](http://markstownsend.github.io/FRisk/index.html) to learn more.
+
+Documentation: http://markstownsend.github.io/FRisk
---
+++ src/FRisk/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("FRisk")>]
+[<assembly: AssemblyProductAttribute("FRisk")>]
+[<assembly: AssemblyDescriptionAttribute("Things I've F(orgotten about)Risk.")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
